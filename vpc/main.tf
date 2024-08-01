8531d0de73ff1a6f8651b97be4b0a4801c7ab346
Talish Garg <talishgarg@umass.edu>
2024-08-01 15:13:54 -0400

    VPC tf

---
+++ vpc/main.tf
@@ -0,0 +1,92 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main_vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = var.public_subnet_1_cidr
+  availability_zone = var.az1
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public_subnet_1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = var.public_subnet_2_cidr
+  availability_zone = var.az2
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public_subnet_2"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = var.az1
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "private_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "main_igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public_rt"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rt_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.main_vpc.id
+}
+
+output "public_subnet_1_id" {
+  value = aws_subnet.public_subnet_1.id
+}
+
+output "public_subnet_2_id" {
+  value = aws_subnet.public_subnet_2.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.igw.id
+}
