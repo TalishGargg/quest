7a95342f6142275ce63240b97de94b22ebcbb4c5
Talish Garg <talishgarg@umass.edu>
2024-08-01 22:40:43 -0400

    EC2 terraform

--- ec2/main.tf
+++ ec2/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "quest_instance" {
   subnet_id     = var.public_subnet_id
   associate_public_ip_address = true
 
-  vpc_security_group_ids = [for sg in aws_security_group.sgs : sg.id]
+  vpc_security_group_ids = [aws_security_group.docker_sg.id, aws_security_group.ssh_sg.id, aws_security_group.http_sg.id]
 
   tags = {
     Name = "Quest"
@@ -17,19 +17,46 @@ resource "aws_instance" "quest_instance" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 80 &
+              # Update the package repository
+              sudo yum update -y
+              
+              # Install Docker
+              sudo yum install -y docker
+              sudo service docker start
+              sudo usermod -a -G docker ec2-user
+              
+              # Install Git
+              sudo yum install -y git
+              
+              # Clone the application repository
+              git clone https://github.com/rearc/quest.git /home/ec2-user/quest
+              cd /home/ec2-user/quest
+
+              # Create Dockerfile
+              cat <<EOT > Dockerfile
+              FROM node:10
+              ENV SECRET_WORD="SOCK_PUPPET"
+              COPY ./quest/ /
+              RUN chmod +x /bin/*
+              RUN npm install
+              EXPOSE 3000
+              CMD ["npm", "start"]
+              EOT
+              
+              # Build and run Docker container
+              sudo docker build -t quest-app .
+              sudo docker run -d -p 3000:3000 quest-app
               EOF
 }
 
-resource "aws_security_group" "sgs" {
-  for_each = toset(var.security_groups)
-
-  vpc_id = var.vpc_id
+resource "aws_security_group" "docker_sg" {
+  name        = "docker_sg"
+  description = "Allow access to Docker"
+  vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = var.ingress_ports[each.key]
-    to_port     = var.ingress_ports[each.key]
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -42,6 +69,73 @@ resource "aws_security_group" "sgs" {
   }
 
   tags = {
-    Name = each.key
+    Name = "docker_sg"
   }
+}
+
+resource "aws_security_group" "ssh_sg" {
+  name        = "ssh_sg"
+  description = "Allow SSH access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh_sg"
+  }
+}
+
+resource "aws_security_group" "http_sg" {
+  name        = "http_sg"
+  description = "Allow HTTP and HTTPS access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "http_sg"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.quest_instance.id
+}
+
+output "public_ip" {
+  value = aws_instance.quest_instance.public_ip
+}
+
+output "private_ip" {
+  value = aws_instance.quest_instance.private_ip
 }
--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,52 +1,32 @@
 variable "aws_region" {
-  description = "The AWS region to create resources in"
+  description = "The AWS region to deploy to"
   type        = string
   default     = "us-east-1"
 }
 
 variable "ami_id" {
-  description = "The ID of the AMI to use for the EC2 instance"
+  description = "The AMI ID to use for the instance"
   type        = string
-  default     = "ami-0ba9883b710b05ac6"
+  default     = "ami-0b9883fb710b05ac6" # Amazon Linux 2023 AMI
 }
 
 variable "instance_type" {
-  description = "The instance type to use for the EC2 instance"
+  description = "The instance type to use for the instance"
   type        = string
   default     = "t2.micro"
 }
 
 variable "key_name" {
-  description = "The name of the key pair to use for the instance"
+  description = "The name of the SSH key pair to use"
   type        = string
-  default     = "my-key"
 }
 
 variable "public_subnet_id" {
-  description = "The ID of the public subnet"
+  description = "The ID of the public subnet to use"
   type        = string
-  default     = "subnet-0b3346acafaa050ce"  # Replace with your subnet ID
 }
 
 variable "vpc_id" {
-  description = "The ID of the VPC"
+  description = "The ID of the VPC to use"
   type        = string
-  default     = "vpc-08e8adcdc71c152a2"  # Replace with your VPC ID
-}
-
-variable "security_groups" {
-  description = "List of security group names"
-  type        = list(string)
-  default     = ["docker", "ssh", "http"]
-}
-
-variable "ingress_ports" {
-  description = "Mapping of security group names to their ingress ports"
-  type        = map(number)
-  default = {
-    docker = 3000
-    ssh    = 22
-    http   = 80
-    https  = 443
-  }
 }
