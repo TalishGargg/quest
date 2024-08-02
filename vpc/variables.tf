eef512bc37ecf8621d6032187db749e11cc8490e
Talish Garg <talishgarg@umass.edu>
2024-08-01 21:22:38 -0400

    VPC variables TF

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-  description = "The AWS region to create things in."
+  description = "The AWS region to deploy the infrastructure."
   default     = "us-east-1"
 }
 
@@ -9,26 +9,26 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnet_1_cidr" {
-  description = "CIDR block for the first public subnet."
+  description = "The CIDR block for the first public subnet."
   default     = "10.0.1.0/24"
 }
 
 variable "public_subnet_2_cidr" {
-  description = "CIDR block for the second public subnet."
-  default     = "10.0.2.0/24"
-}
-
-variable "private_subnet_cidr" {
-  description = "CIDR block for the private subnet."
+  description = "The CIDR block for the second public subnet."
   default     = "10.0.3.0/24"
 }
 
+variable "private_subnet_cidr" {
+  description = "The CIDR block for the private subnet."
+  default     = "10.0.2.0/24"
+}
+
 variable "az1" {
-  description = "Availability Zone for the first public subnet."
+  description = "The first availability zone."
   default     = "us-east-1a"
 }
 
 variable "az2" {
-  description = "Availability Zone for the second public subnet."
+  description = "The second availability zone."
   default     = "us-east-1b"
 }
