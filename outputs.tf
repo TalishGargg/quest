c4b96b2c91ab038d90346a44270914fcca13237d
Talish Garg <talishgarg@umass.edu>
2024-08-02 14:01:08 -0400

    Root level outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -14,26 +14,42 @@ output "private_subnet_id" {
   value = module.vpc.private_subnet_id
 }
 
-output "internet_gateway_id" {
-  value = module.vpc.internet_gateway_id
+output "docker_sg_id" {
+  value = module.security_groups.docker_sg_id
 }
 
-output "public_route_table_id" {
-  value = module.vpc.public_route_table_id
+output "http_sg_id" {
+  value = module.security_groups.http_sg_id
 }
 
-output "private_route_table_id" {
-  value = module.vpc.private_route_table_id
+output "ssh_sg_id" {
+  value = module.security_groups.ssh_sg_id
 }
 
-output "ec2_instance_id" {
-  value = module.ec2.ec2_instance_id
+output "instance_id" {
+  value = module.ec2.instance_id
+}
+
+output "instance_public_ip" {
+  value = module.ec2.instance_public_ip
 }
 
 output "load_balancer_arn" {
   value = module.load_balancer.load_balancer_arn
 }
 
-output "http_sg_id" {
-  value = module.security_groups.http_sg_id
+output "http_listener_arn" {
+  value = module.load_balancer.http_listener_arn
+}
+
+output "https_listener_arn" {
+  value = module.load_balancer.https_listener_arn
+}
+
+output "quest_http_target_group_arn" {
+  value = module.load_balancer.quest_http_target_group_arn
+}
+
+output "ecs_quest_target_group_arn" {
+  value = module.load_balancer.ecs_quest_target_group_arn
 }
