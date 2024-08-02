5509b65a324a5010ae011d48919df37d25933b8d
Talish Garg <talishgarg@umass.edu>
2024-08-01 21:23:53 -0400

    VPC outputs tf file

--- vpc/main.tf
+++ vpc/main.tf
@@ -73,7 +73,6 @@ resource "aws_route_table_association" "public_rt_assoc_2" {
 
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main_vpc.id
-
   tags = {
     Name = "private_rt"
   }
@@ -82,32 +81,4 @@ resource "aws_route_table" "private_rt" {
 resource "aws_route_table_association" "private_rt_assoc" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_rt.id
-}
-
-output "vpc_id" {
-  value = aws_vpc.main_vpc.id
-}
-
-output "public_subnet_1_id" {
-  value = aws_subnet.public_subnet_1.id
-}
-
-output "public_subnet_2_id" {
-  value = aws_subnet.public_subnet_2.id
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.private_subnet.id
-}
-
-output "internet_gateway_id" {
-  value = aws_internet_gateway.igw.id
-}
-
-output "public_route_table_id" {
-  value = aws_route_table.public_rt.id
-}
-
-output "private_route_table_id" {
-  value = aws_route_table.private_rt.id
 }
