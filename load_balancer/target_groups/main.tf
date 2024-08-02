4b044aca666115d52a375ff025a3795566194770
Talish Garg <talishgarg@umass.edu>
2024-08-02 12:08:34 -0400

    Target group outputs

--- load_balancer/target_groups/main.tf
+++ load_balancer/target_groups/main.tf
@@ -43,11 +43,3 @@ resource "aws_lb_target_group" "ecs_quest" {
     Name = "ecs-quest"
   }
 }
-
-output "quest_http_target_group_arn" {
-  value = aws_lb_target_group.quest_http.arn
-}
-
-output "ecs_quest_target_group_arn" {
-  value = aws_lb_target_group.ecs_quest.arn
-}
