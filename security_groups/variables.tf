f7f2f8fe6539f4357a0f54e34d311c0f44c6d9c1
Talish Garg <talishgarg@umass.edu>
2024-08-02 21:12:25 -0400

    Variables updated

--- security_groups/variables.tf
+++ security_groups/variables.tf
@@ -1,6 +1,7 @@
 variable "vpc_id" {
   description = "The VPC ID"
   type        = string
+  default     = "vpc-08e8adcdc71c152a2"
 }
 
 variable "docker_port" {
@@ -30,5 +31,5 @@ variable "ssh_port" {
 variable "allowed_ssh_ip" {
   description = "Allowed IP for SSH"
   type        = string
-  default     = 0.0.0.0
+  default     = "38.15.229.171/32"
 }
