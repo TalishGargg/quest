e4f8441306ce96a0f0516643e16fd02fdc694649
TalishGargg <67477834+TalishGargg@users.noreply.github.com>
2024-08-03 16:50:41 -0400

    Update variables.tf

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -2,3 +2,33 @@ variable "aws_region" {
   description = "The AWS region to deploy resources."
   default     = "us-east-1"
 }
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_1_cidr" {
+  description = "The CIDR block for the first public subnet."
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+  description = "The CIDR block for the second public subnet."
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "az1" {
+  description = "The availability zone for the first public subnet."
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "az2" {
+  description = "The availability zone for the second public subnet."
+  type        = string
+  default     = "us-east-1b"
+}
