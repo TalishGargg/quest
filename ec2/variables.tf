fa96da603a8281f5ea954e93b1dda615064ac630
Talish Garg <talishgarg@umass.edu>
2024-08-03 10:46:29 -0400

    Changes

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -4,33 +4,10 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "ami_id" {
-  description = "The AMI ID to use for the instance"
-  type        = string
-  default     = "ami-0b9883fb710b05ac6" # Amazon Linux 2023 AMI
-}
-
 variable "instance_type" {
   description = "The instance type to use for the instance"
   type        = string
   default     = "t2.micro"
-}
-
-variable "key_name" {
-  description = "The name of the SSH key pair to use"
-  type        = string
-  default     = "quest"
-}
-
-variable "public_subnet_id" {
-  description = "The ID of the public subnet to use"
-  type        = string
-  default     = "subnet-0b3346acafaa050ce"
-}
-
-variable "vpc_id" {
-  description = "The ID of the VPC to use"
-  type        = "vpc-08e8adcdc71c152a2"
 }
 
 variable "ssh_port" {
