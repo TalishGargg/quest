567b14157f8c00620b3429803f7af118df3db62e
Talish Garg <talishgarg@umass.edu>
2024-08-04 00:18:58 -0400

    IAM for pushing self signed cert to ACM

--- iam/main.tf
+++ iam/main.tf
@@ -7,13 +7,13 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
-        }
+        },
         Action = "sts:AssumeRole"
       }
     ]
@@ -33,16 +33,16 @@ resource "aws_iam_policy" "ecr_access_policy" {
   description = "Policy to allow ECS tasks to pull from ECR"
 
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Action = [
           "ecr:GetAuthorizationToken",
           "ecr:BatchCheckLayerAvailability",
           "ecr:GetDownloadUrlForLayer",
           "ecr:BatchGetImage"
-        ]
+        ],
         Resource = "*"
       }
     ]
@@ -59,13 +59,13 @@ resource "aws_iam_role" "ec2_instance_role" {
   name = "ec2InstanceRole"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Principal = {
           Service = "ec2.amazonaws.com"
-        }
+        },
         Action = "sts:AssumeRole"
       }
     ]
@@ -109,3 +109,26 @@ resource "aws_iam_role_policy_attachment" "ec2_instance_role_policy_attachment" 
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
 }
 
+# Attach ACM permissions to EC2 instance role
+resource "aws_iam_policy" "acm_policy" {
+  name        = "ACMPolicy"
+  description = "Policy to allow EC2 to import certificates to ACM"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "acm:ImportCertificate",
+          "acm:DescribeCertificate"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "acm_policy_attachment" {
+  role       = aws_iam_role.ec2_instance_role.name
+  policy_arn = aws_iam_policy.acm_policy.arn
+}
