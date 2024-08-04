30924747b3e85361081f20780450061d1cbc0855
TalishGargg <67477834+TalishGargg@users.noreply.github.com>
2024-08-03 21:21:58 -0400

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -21,8 +21,3 @@ variable "allowed_ssh_ip" {
   type        = string
   default     = "0.0.0.0/0"
 }
-
-variable "ecr_repo_url" {
-  description = "The URL of the ECR repository"
-  type        = string
-}
