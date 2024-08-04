5ad2fe86c2dc0f1adda3fb7fd201c99d268b35cc
Talish Garg <talishgarg@umass.edu>
2024-08-04 09:54:12 -0400

    Changed main file

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ module "ecs_cluster_and_service" {
     module.vpc.public_subnet_1_id, 
     module.vpc.public_subnet_2_id
   ]
-  security_group_id  = module.security_groups.http_sg_id
+  http_sg_id  = module.security_groups.http_sg_id
   target_group_arn   = module.load_balancer.quest_http_target_group_arn
   certificate_arn    = module.ec2.certificate_arn
   vpc_id             = module.vpc.vpc_id
