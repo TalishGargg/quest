e65b263560cf61fe36cdb4a9dc7c97172946568c
TalishGargg <67477834+TalishGargg@users.noreply.github.com>
2024-08-05 19:02:39 -0400

    Update main.tf

--- ecs/main.tf
+++ ecs/main.tf
@@ -7,7 +7,7 @@ resource "aws_ecs_cluster" "quest_cluster" {
 }
 
 resource "aws_ecs_service" "quest_service" {
-  name            = "quest-task"
+  name            = "quest-ecs"
   cluster         = aws_ecs_cluster.quest_cluster.id
   task_definition = var.task_definition_arn
   desired_count   = 1
@@ -15,16 +15,17 @@ resource "aws_ecs_service" "quest_service" {
 
   network_configuration {
     subnets          = var.public_subnet_ids
-    security_groups  = [var.http_sg_id]
+    security_groups  = [var.http_sg_id, var.docker_sg_id]
     assign_public_ip = true
   }
 
   load_balancer {
-    target_group_arn = var.ecs_quest_target_group_arn  # Reference the correct target group
+    target_group_arn = var.ecs_quest_target_group_arn
     container_name   = "quest-app"
     container_port   = 3000
   }
 
   deployment_maximum_percent         = 200
   deployment_minimum_healthy_percent = 100
+
 }
