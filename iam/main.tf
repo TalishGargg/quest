a2e322e7221b8ebe6f69112467535069fd4b04e6
TalishGargg <67477834+TalishGargg@users.noreply.github.com>
2024-08-04 22:38:16 -0400

    Update main.tf

--- iam/main.tf
+++ iam/main.tf
@@ -131,7 +131,25 @@ resource "aws_iam_policy" "acm_policy" {
   })
 }
 
+# Attach the ACM policy to a suitable role that handles ACM operations
+resource "aws_iam_role" "acm_role" {
+  name = "acmRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "acm.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "acm_policy_attachment" {
-  role       = aws_iam_role.ec2_instance_role.name
+  role       = aws_iam_role.acm_role.name
   policy_arn = aws_iam_policy.acm_policy.arn
 }
