ad41cd7dd64baf1468a348517fdfd0eff22735e3
TalishGargg <67477834+TalishGargg@users.noreply.github.com>
2024-08-05 19:05:43 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,10 +51,11 @@ module "ecs_cluster_and_service" {
     module.vpc.public_subnet_2_id
   ]
   http_sg_id          = module.security_groups.http_sg_id
-  target_group_arn    = module.load_balancer.quest_http_target_group_arn
+  docker_sg_id        = module.security_groups.docker_sg_id
+  target_group_arn =    module.load_balancer.ecs_quest_target_group_arn
+  ecs_quest_target_group_arn = module.load_balancer.ecs_quest_target_group_arn
   certificate_arn     = module.certificate.self_signed_cert_arn
   vpc_id              = module.vpc.vpc_id
-  ecs_quest_target_group_arn = module.load_balancer.ecs_quest_target_group_arn
 }
 
 module "ecr" {
