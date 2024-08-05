1979882f3122106d3e3ac62b7197833fdb2c0390
TalishGargg <67477834+TalishGargg@users.noreply.github.com>
2024-08-05 10:00:21 -0400

    Update main.tf

--- certificate/main.tf
+++ certificate/main.tf
@@ -1,32 +1,58 @@
-resource "tls_private_key" "example" {
+resource "tls_private_key" "ca_key" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
 
-resource "time_static" "now" {}
-
-resource "tls_self_signed_cert" "example" {
-  private_key_pem = tls_private_key.example.private_key_pem
-
-  is_ca_certificate = false
+resource "tls_self_signed_cert" "ca_cert" {
+  private_key_pem = tls_private_key.ca_key.private_key_pem
+  is_ca_certificate = true
 
   subject {
-    common_name  = "example.com"
-    organization = "Example Organization"
+    common_name  = "example-ca"
+    organization = "Example CA Organization"
   }
 
-  validity_period_hours  = 8760
+  validity_period_hours = 8760
+
+  allowed_uses = [
+    "cert_signing",
+    "crl_signing",
+    "digital_signature",
+  ]
+}
+
+resource "tls_private_key" "server_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "tls_cert_request" "server_csr" {
+  private_key_pem = tls_private_key.server_key.private_key_pem
+
+  subject {
+    common_name  = var.load_balancer_dns_name
+    organization = "Example Organization"
+  }
+}
+
+resource "tls_locally_signed_cert" "server_cert" {
+  cert_request_pem = tls_cert_request.server_csr.cert_request_pem
+  ca_private_key_pem = tls_private_key.ca_key.private_key_pem
+  ca_cert_pem = tls_self_signed_cert.ca_cert.cert_pem
+
+  validity_period_hours = 8760
 
   allowed_uses = [
     "key_encipherment",
     "digital_signature",
-    "server_auth"
+    "server_auth",
   ]
 }
 
 resource "aws_acm_certificate" "self_signed_cert" {
-  private_key       = tls_private_key.example.private_key_pem
-  certificate_body  = tls_self_signed_cert.example.cert_pem
+  private_key       = tls_private_key.server_key.private_key_pem
+  certificate_body  = tls_locally_signed_cert.server_cert.cert_pem
+  certificate_chain = tls_self_signed_cert.ca_cert.cert_pem
   tags = {
     Name = "self_signed_cert"
   }
