7df613f4a2e081f69e3653a872d6c854e6223fd9
TalishGargg <67477834+TalishGargg@users.noreply.github.com>
2024-08-05 00:27:14 -0400

    Update main.tf

--- certificate/main.tf
+++ certificate/main.tf
@@ -3,14 +3,20 @@ resource "tls_private_key" "example" {
   rsa_bits  = 2048
 }
 
+resource "time_static" "now" {}
+
 resource "tls_self_signed_cert" "example" {
   private_key_pem = tls_private_key.example.private_key_pem
+
+  is_ca_certificate = false
+
   subject {
     common_name  = "example.com"
     organization = "Example Organization"
   }
-  validity_period_hours = 8760
-  is_ca_certificate     = false
+
+  validity_period_hours  = 8760
+
   allowed_uses = [
     "key_encipherment",
     "digital_signature",
@@ -25,3 +31,4 @@ resource "aws_acm_certificate" "self_signed_cert" {
     Name = "self_signed_cert"
   }
 }
+
