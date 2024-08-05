ee91bba1e38e7a5a19fea3e3fb1e013265ad6a7b
TalishGargg <67477834+TalishGargg@users.noreply.github.com>
2024-08-05 19:04:15 -0400

    Update outputs.tf

--- load_balancer/outputs.tf
+++ load_balancer/outputs.tf
@@ -1,3 +1,18 @@
+output "http_listener_arn" {
+  description = "The ARN of the HTTP listener"
+  value       = aws_lb_listener.http.arn
+}
+
+output "https_listener_arn" {
+  description = "The ARN of the HTTPS listener"
+  value       = aws_lb_listener.https.arn
+}
+
+output "load_balancer_arn" {
+  description = "The ARN of the load balancer"
+  value       = aws_lb.quest_alb.arn
+}
+
 output "load_balancer_dns_name" {
   description = "The DNS name of the load balancer"
   value       = aws_lb.quest_alb.dns_name
@@ -10,8 +25,3 @@ output "quest_http_target_group_arn" {
 output "ecs_quest_target_group_arn" {
   value = aws_lb_target_group.ecs_quest.arn
 }
-
-output "quest_alb_arn" {
-  value = aws_lb.quest_alb.arn
-}
-
