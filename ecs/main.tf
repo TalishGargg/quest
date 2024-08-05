63a165ed29c0fb0fae919943a9176007e108d158
TalishGargg <67477834+TalishGargg@users.noreply.github.com>
2024-08-05 10:35:08 -0400

    Update main.tf

--- ecs/main.tf
+++ ecs/main.tf
@@ -20,77 +20,11 @@ resource "aws_ecs_service" "quest_service" {
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.ecs_quest.arn  # Reference the correct target group
+    target_group_arn = var.ecs_quest_target_group_arn  # Reference the correct target group
     container_name   = "quest-app"
     container_port   = 3000
   }
 
   deployment_maximum_percent         = 200
   deployment_minimum_healthy_percent = 100
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.quest_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.quest_http.arn
-  }
-}
-
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.quest_alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-  certificate_arn   = var.certificate_arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.quest_http.arn
-  }
-}
-
-resource "aws_lb_target_group" "quest_http" {
-  name        = "quest-http"
-  port        = 3000
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "instance"
-
-  health_check {
-    interval            = 30
-    path                = "/"
-    protocol            = "HTTP"
-    timeout             = 5
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-  }
-
-  tags = {
-    Name = "quest-http"
-  }
-}
-
-resource "aws_lb_target_group" "ecs_quest" {
-  name        = "ecs-quest"
-  port        = 3000
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"
-
-  health_check {
-    interval            = 30
-    path                = "/"
-    protocol            = "HTTP"
-    timeout             = 5
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-  }
-
-  tags = {
-    Name = "ecs-quest"
-  }
 }
