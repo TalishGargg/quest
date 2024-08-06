a2d3d8094d2a00f3245242fa0d8b3498fc810594
TalishGargg <67477834+TalishGargg@users.noreply.github.com>
2024-08-05 22:43:50 -0400

    Update main.tf

--- load_balancer/target_groups/main.tf
+++ load_balancer/target_groups/main.tf
@@ -2,27 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_lb_target_group" "quest_http" {
-  name        = "quest-http"
-  port        = var.port
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "instance"
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200-299"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-
-  tags = {
-    Name = "quest-http"
-  }
-}
-
 resource "aws_lb_target_group" "ecs_quest" {
   name        = "ecs-quest"
   port        = var.port
