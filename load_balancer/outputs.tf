0e2a775302215f2d2b9fb154a11849ae7df2c77f
TalishGargg <67477834+TalishGargg@users.noreply.github.com>
2024-08-05 22:43:26 -0400

    Update outputs.tf

--- load_balancer/outputs.tf
+++ load_balancer/outputs.tf
@@ -18,10 +18,6 @@ output "load_balancer_dns_name" {
   value       = aws_lb.quest_alb.dns_name
 }
 
-output "quest_http_target_group_arn" {
-  value = aws_lb_target_group.quest_http.arn
-}
-
 output "ecs_quest_target_group_arn" {
   value = aws_lb_target_group.ecs_quest.arn
 }
