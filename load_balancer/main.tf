e7a93aafbe7116a6671c938f94288650f8a91883
TalishGargg <67477834+TalishGargg@users.noreply.github.com>
2024-08-05 22:43:09 -0400

    Update main.tf

--- load_balancer/main.tf
+++ load_balancer/main.tf
@@ -20,19 +20,8 @@ resource "aws_lb_listener" "http" {
   protocol          = "HTTP"
 
   default_action {
-    type = "forward"
-
-    forward {
-      target_group {
-        arn    = aws_lb_target_group.quest_http.arn
-        weight = 50
-      }
-
-      target_group {
-        arn    = aws_lb_target_group.ecs_quest.arn
-        weight = 50
-      }
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_quest.arn
   }
 }
 
@@ -44,40 +33,8 @@ resource "aws_lb_listener" "https" {
   certificate_arn   = var.certificate_arn
 
   default_action {
-    type = "forward"
-
-    forward {
-      target_group {
-        arn    = aws_lb_target_group.quest_http.arn
-        weight = 50
-      }
-
-      target_group {
-        arn    = aws_lb_target_group.ecs_quest.arn
-        weight = 50
-      }
-    }
-  }
-}
-
-resource "aws_lb_target_group" "quest_http" {
-  name        = "quest-http"
-  port        = 3000
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "instance"
-
-  health_check {
-    interval            = 30
-    path                = "/"
-    protocol            = "HTTP"
-    timeout             = 5
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-  }
-
-  tags = {
-    Name = "quest-http"
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_quest.arn
   }
 }
 
